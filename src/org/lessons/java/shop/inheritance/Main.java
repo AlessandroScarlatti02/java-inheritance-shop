3d5345839a2539e5ca1b2e20920fee574679f258
AlessandroScarlatti02 <alessandro.scarlatti2002@gmail.com>
2024-10-02 16:27:45 +0200

    Test all Products type on main

--- src/org/lessons/java/shop/inheritance/Main.java
+++ src/org/lessons/java/shop/inheritance/Main.java
@@ -4,20 +4,25 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Product p1 = new Product(0, "Hisense", "Hisense", 399.99, 1.22);
+		//Prova Oggetto Product e stampa informazioni
+		Product p1 = new Product(0, "T-Shirt", "Nike", 19.99, 1.22);
 		p1.getProductInformations(p1);
 			
-		double completePrice;
 		
-		completePrice= p1.getCompletePrice();
-		System.out.println(completePrice);
 		
+		//Prova Oggetto TV e stampa informazioni
 		Tv t1 = new Tv(55, true, 0, "TV OLED", "LG", 895.00, 1.22);
 		t1.getProductInformations(t1);
 
+		
+		
+		//Prova Oggetto Smartphone e stampa informazioni
 		Smartphone s1 = new Smartphone(0, 1024, 0, "Iphone 16 Pro Max", "Apple", 1150, 1.22);
 		s1.getProductInformations(s1);
 		
+		
+		
+		//Prova Oggetto Headphones e stampa informazioni
 		Headphones h1 = new Headphones("Blue", true, 0, "Bose QuietComfort", "Bose", 298.99, 1.22);
 		h1.getProductInformations(h1);
 	}
